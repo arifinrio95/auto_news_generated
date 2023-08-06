916906ddce50adc6e01f0fee4a1c71cd4d5332a2
arifinrio95 <59592379+arifinrio95@users.noreply.github.com>
2023-08-06 23:34:15 +0700

    Update auto_news.py

--- auto_news.py
+++ auto_news.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import requests
 import json
+from datetime import datetime
 
 st.title("Berita dari AI")
 
 st.markdown("Aplikasi ini menggunakan API dari News API untuk mengambil berita terbaru di Indonesia dan WebPilot API untuk melakukan parafrase dalam gaya non formal.")
 
-def fetch_news(from_date, to_date):
-    url = ('https://newsapi.org/v2/everything?'
+def fetch_news():
+    url = ('https://newsapi.org/v2/top-headlines?'
            'country=id&'
-           'from={}&'
-           'to={}&'
-           'apiKey={}'.format(from_date, to_date, st.secrets['api_key']))
+           'apiKey={}'.format(st.secrets['api_key']))
     response_news = requests.get(url)
     return response_news.json()['articles']
 
@@ -28,29 +27,27 @@ def paraphrase_article(article_url):
     response = requests.post(endpoint, headers=headers, data=json.dumps(data))
     return response.json().get("content")
 
-from_date = st.date_input('Pilih tanggal mulai:')
-to_date = st.date_input('Pilih tanggal akhir:')
+st.markdown("### Mengambil Berita...")
+articles = fetch_news()
+dates = sorted(list(set(article['publishedAt'].split('T')[0] for article in articles)), reverse=True)
+selected_date = st.selectbox("Pilih Tanggal Berita yang Ingin Diparafrase:", dates)
 
-if from_date and to_date:
-    st.markdown("### Mengambil Berita...")
-    articles = fetch_news(from_date, to_date)
-    article_titles = ["Pilih Berita"] + [article['title'] for article in articles]
-    selected_title = st.selectbox("Pilih Judul Berita yang Ingin Diparafrase:", article_titles)
+selected_articles = [article for article in articles if article['publishedAt'].split('T')[0] == selected_date]
+article_titles = ["Pilih Berita"] + [article['title'] for article in selected_articles]
+selected_title = st.selectbox("Pilih Judul Berita yang Ingin Diparafrase:", article_titles)
 
-    selected_article = None
-    if selected_title != "Pilih Berita":
-        selected_article = next((article for article in articles if article['title'] == selected_title), None)
+selected_article = None
+if selected_title != "Pilih Berita":
+    selected_article = next((article for article in selected_articles if article['title'] == selected_title), None)
 
-    if selected_article:
-        st.header("Judul Berita Terpilih:")
-        st.write(selected_article['title'])
-        st.image(selected_article['urlToImage'], caption=selected_article['title'])
-        st.markdown("[Baca berita asli]({})".format(selected_article['url']))
+if selected_article:
+    st.header("Judul Berita Terpilih:")
+    st.write(selected_article['title'])
+    st.image(selected_article['urlToImage'], caption=selected_article['title'])
+    st.markdown("[Baca berita asli]({})".format(selected_article['url']))
 
-        st.header("Parafrase dalam Gaya Non Formal:")
-        paraphrased_content = paraphrase_article(selected_article['url'])
-        st.write(paraphrased_content)
-    else:
-        st.markdown("Pilih berita dari dropdown di atas untuk mulai menghasilkan parafrase.")
+    st.header("Parafrase dalam Gaya Non Formal:")
+    paraphrased_content = paraphrase_article(selected_article['url'])
+    st.write(paraphrased_content)
 else:
-    st.markdown("Pilih rentang tanggal untuk melanjutkan.")
+    st.markdown("Pilih berita dari dropdown di atas untuk mulai menghasilkan parafrase.")
